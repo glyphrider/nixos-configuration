cb41452757554430168732120b0bfa89f9c09ea8
Brian H. Ward <glyphrider@gmail.com>
2024-03-26 09:44:34 -0400

    added some (helpful?) comments to the extra-filesystems config

--- beast-extra-filesystems-configuration.nix
+++ beast-extra-filesystems-configuration.nix
@@ -1,11 +1,11 @@
 { config, lib, pkgs, ... }:
 {
   fileSystems = {
-    "/home/brian/Games" = {
+    "/home/brian/Games" = { # Games is an additional Steam Library, so I can reinstall the OS without having to reinstall Steam Games
       depends = [ "/" "/home" ];
       device = "/dev/disk/by-uuid/d1a11a3d-52f2-4f88-bef5-c1abcb68c56f";
     };
-#    "/var/lib/libvirt" = {
+#    "/var/lib/libvirt" = { # This is an volume to keep VMs persistant across OS reinstalls (only somewhat effective)
 #      depends = [ "/" "/var" ];
 #      device = "/dev/disk/by-uuid/3ec5b0fb-b766-40e4-982e-fd948fe52495";
 #    };
